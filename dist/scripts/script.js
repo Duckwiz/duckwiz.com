96b5d6bc286870d136188e2a53c9c32581f7ad26
Duckwiz <duckwizard906@gmail.com>
2023-05-30 16:01:43 -0400

    parse?

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -4,7 +4,7 @@
 async function getHello() {
   const res = await fetch("/api/hello");
   console.log(res);
-  document.getElementById("msg").textContent = res;
+  document.getElementById("msg").textContent = res.body;
 };
 
 getHello();
