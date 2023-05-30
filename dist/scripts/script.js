ba9fcc3a8ba7cd23c557105ffa605a9c6802812f
Duckwiz <duckwizard906@gmail.com>
2023-05-30 15:15:35 -0400

    fdsfsd

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,9 +1,2 @@
 const num = Math.floor(Math.random()*1000)
 document.getElementById("val").textContent = num;
-
-const options = {
-  method: "POST",
-  body: JSON.stringify(num)
-}
-
-fetch("/.netlify/functions/index", options)
