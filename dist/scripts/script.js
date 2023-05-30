31c536d3ec5b9503485aa3f2c315d36483d303c5
Duckwiz <duckwizard906@gmail.com>
2023-05-30 16:06:58 -0400

    dgsfsd

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -2,7 +2,7 @@
 // document.getElementById("val").textContent = num;
 
 async function getHello() {
-  const res = await fetch("duckwiz.com/api/hello");
+  const res = await fetch("/api/hello");
   console.log(res.json);
   document.getElementById("msg").textContent = res;
 };
