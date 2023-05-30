0fceb2aa1b34d1ba052e98432aa8609b40c70ade
Duckwiz <duckwizard906@gmail.com>
2023-05-30 16:47:24 -0400

    uum

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,10 +1,11 @@
 // const num = Math.floor(Math.random()*1000)
 // document.getElementById("val").textContent = num;
 
-async function getHello() {
-  const res = await fetch("/api/hello");
-  console.log(res);
-  document.getElementById("msg").textContent = res;
-};
-
-getHello();
+fetch('/api/hello')
+  .then(response => response.text())
+  .then(data => {
+    console.log('Data from /hello route:', data);
+  })
+  .catch(error => {
+    console.error('Error fetching /hello route:', error);
+  });
