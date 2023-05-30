e476da6e219cbc4388826b3795cefe7c740513ce
Duckwiz <duckwizard906@gmail.com>
2023-05-30 15:46:11 -0400

    pls work

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,5 +1,9 @@
 // const num = Math.floor(Math.random()*1000)
 // document.getElementById("val").textContent = num;
 
-const msg = await fetch("/hello");
+const msg = async () => {
+  const res = await fetch("/hello");
+  return res;
+}
+
 document.getElementById("msg").textContent = msg;
