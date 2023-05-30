366a603d7f46802a34859efeacc652c85feed437
Duckwiz <duckwizard906@gmail.com>
2023-05-30 18:25:47 -0400

    i dont know

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,1 +1,7 @@
-fetch("/api/hello.js");
+async function getHello() {
+  const res = await fetch("/api/hello");
+  console.log(res);
+  document.getElementById("msg").textContent = res;
+};
+
+getHello();
