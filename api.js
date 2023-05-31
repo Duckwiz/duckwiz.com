66a3652dda85cd1b8d864462a93ca46b06e80036
Duckwiz <duckwizard906@gmail.com>
2023-05-31 17:43:36 -0400

    done?

--- api.js
+++ api.js
@@ -6,6 +6,10 @@ app.listen(port, () => console.log(`listening on port ${port}!`));
 
 app.use(express.static('public'));
 
+app.get("/about", (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/about.html`)
+})
+
 app.get("/game", (req, res) => {
     res.sendFile(`${__dirname}/public/pages/game.html`)
-  })
+})
