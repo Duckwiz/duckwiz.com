85e94ec432508bd969a063c1d021c2bbce2c747d
Duckwiz <duckwizard906@gmail.com>
2023-05-31 15:49:33 -0400

    fix links

--- api.js
+++ api.js
@@ -4,36 +4,4 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}!`));
 
-// app.get("/", async (req, res) => res.type('html').send(await readFile("/public/index.html", utf8)));
-
 app.use(express.static('public'));
-
-// app.get("/", (req, res) => res.type('html').send(html));
-// const html = `
-// <!DOCTYPE html>
-// <html>
-//     <head>
-//       <link rel="icon" href="/images/icon.png">
-//       <link href ="/style/style.css" rel="stylesheet">
-//       <title>duckwiz</title>
-//       <script type="text/javascript" src="/scripts/script.js" defer></script>
-//     </head>
-
-//     <body>
-//       <header>
-//         <img src="/images/logo.png" alt="logo" id="logo">
-//         <nav>
-//           <a href="/index">home</a>
-//           <a href="/pages/about">about</a>
-//           <a href="/pages/game">game</a>
-//         </nav>
-//       </header>
-//       <main>
-//         <h1>Welome!!!!!!</h1>
-//         <p>here: <span id="msg">-</span></p>
-//       </main>
-//       <footer>
-//       </footer>
-//     </body>
-// </html>
-// `
