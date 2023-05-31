89f0308eeac48b5c080d057730ff5c4eb30dafb0
Duckwiz <duckwizard906@gmail.com>
2023-05-31 18:39:12 -0400

    msg

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,8 @@
 async function getHello() {
   const res = await fetch("/hello");
   const data = await res.json();
-  console.log(data);
-  document.getElementById("msg").textContent = data;
+  console.log(data.msg);
+  document.getElementById("msg").textContent = data.msg;
 };
 
 getHello();
