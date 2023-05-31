d554a8d7bc80f371be4665a32f67043b7dbeb2ab
Duckwiz <duckwizard906@gmail.com>
2023-05-31 15:23:16 -0400

    dunno

--- api.js
+++ api.js
@@ -4,4 +4,4 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}!`));
 
-app.get("/", async (req, res) => res.send(await readFile("./public/index.html", utf8)));
+app.get("/", async (req, res) => res.send(await readFile("/public/index.html", utf8)));
