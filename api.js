3d53020864780ee0f518dbbb8cab7cf2125e5e6a
Duckwiz <duckwizard906@gmail.com>
2023-05-31 17:09:41 -0400

    retry

--- api.js
+++ api.js
@@ -4,4 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}!`));
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+    response.sendFile("/public/index.html")
+  })
