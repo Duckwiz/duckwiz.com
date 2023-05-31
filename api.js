4fc47a188277604e7255afe6385b0789b865bfe6
Duckwiz <duckwizard906@gmail.com>
2023-05-31 15:09:52 -0400

    pls serve site

--- api.js
+++ api.js
@@ -1,33 +1,4 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-const html = `
-<!DOCTYPE html>
-<html>
-    <head>
-      <link rel="icon" href="/images/icon.png">
-      <link href ="/style/style.css" rel="stylesheet">
-      <title>duckwiz</title>
-      <script type="text/javascript" src="/scripts/script.js" defer></script>
-    </head>
-
-    <body>
-      <header>
-        <img src="/images/logo.png" alt="logo" id="logo">
-        <nav>
-          <a href="/index">home</a>
-          <a href="/pages/about">about</a>
-          <a href="/pages/game">game</a>
-        </nav>
-      </header>
-      <main>
-        <h1>Welome!!!!!!</h1>
-        <p>here: <span id="msg">-</span></p>
-      </main>
-      <footer>
-      </footer>
-    </body>
-</html>
-`
+app.get("/", async (req, res) => res.send(await readFile("./public/index", utf8)));
