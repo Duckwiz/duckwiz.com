6f20c0141f3845856909f2c90352efc7edf9f37a
Duckwiz <mcardenas5926@gmail.com>
2024-11-14 18:34:45 -0500

    fix?

--- public/scripts/headermanager.js
+++ public/scripts/headermanager.js
@@ -1,12 +1,12 @@
 class CustomHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            < img src = "/images/logo.png" alt = "logo" id = "logo" >
-                < nav >
+            <img src = "/images/logo.png" alt = "logo" id = "logo">
+                <nav>
                     <a href="/">home</a>
                     <a href="/game">game</a>
                     <a href="/kins">sillies</a>
-                </nav >
+                </nav>
             `
     }
 }
