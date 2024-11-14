04ce00f6c94f081b4aaf82517f566b0516d0d0f1
Duckwiz <mcardenas5926@gmail.com>
2024-11-14 18:37:03 -0500

    please

--- public/scripts/headermanager.js
+++ public/scripts/headermanager.js
@@ -1,7 +1,7 @@
 class CustomHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <img src = "/images/logo.png" alt = "logo" id = "logo">
+            <img src="/images/logo.png" alt="logo" id="logo">
                 <nav>
                     <a href="/">home</a>
                     <a href="/game">game</a>
