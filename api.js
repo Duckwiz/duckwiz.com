c33297c4abefcaec95872662faf4f72fe17b7553
Duckwiz <mcardenas5926@gmail.com>
2024-11-14 21:02:51 -0500

    PLEASE

--- api.js
+++ api.js
@@ -20,8 +20,8 @@ app.get("/sillies", (req, res) => {
 app.get("/hello", (req, res) => {
     fs.readFile(`/data/visits.txt`, "utf8", (err, data) => {
         visits = parseInt(data) + 1;
-    })
-    fs.writeFile(`/data/visits.txt`, visits.toString(), (err) => {
-        res.json({ "msg": visits })
+        fs.writeFile(`/data/visits.txt`, visits.toString(), (err) => {
+            res.json({ "msg": visits })
+        })
     })
 })
