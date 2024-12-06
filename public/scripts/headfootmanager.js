6d4d3bcb0b003ad2e87f74a99a5d06bd7200ec7f
Duckwiz <mcardenas5926@gmail.com>
2024-12-05 23:21:09 -0500

    footers

---
+++ public/scripts/headfootmanager.js
@@ -0,0 +1,29 @@
+class CustomHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <img src="/images/logo.png" alt="logo" id="logo">
+                <nav>
+                    <a href="/">home</a>
+                    <a href="/sillies">sillies</a>
+                </nav>
+            </header>
+            `
+    }
+}
+
+customElements.define('custom-header', CustomHeader)
+
+
+class CustomFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <hr>
+                <p>&#9<a href="https://bsky.app/profile/duckwiz.com">bluesky</a>&#9<a href="https://duckwiz.itch.io/">itch</a></p>
+            </footer>
+            `
+    }
+}
+
+customElements.define('custom-footer', CustomFooter)
