9c4ac7795a8bec54e3358be1052a87b5c0f84aa1
Duckwiz <mcardenas5926@gmail.com>
2025-02-03 15:36:09 -0500

    fix the visits pls

--- api.js
+++ api.js
@@ -17,7 +17,7 @@ app.get("/projects", (req, res) => {
     res.sendFile(`${__dirname}/public/pages/projects.html`)
 })
 
-app.get("hello", (req, res) => {
+app.get("/hello", (req, res) => {
     fs.readFile(`/data/visits.txt`, "utf8", (err, data) => {
         visits = parseInt(data) + 1;
         fs.writeFile(`/data/visits.txt`, visits.toString(), (err) => {
